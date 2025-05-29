f7c83b08ec2f72ea59bf52fd5b204e1934c3ade5
suzhanlee <wlscww@kakao.com>
2025-05-29 18:00:10 +0900

    scr 로 폴더 구조 수정

--- crewbe-interview-android/src/config/aws.config.ts
+++
@@ -1,8 +0,0 @@
-import Constants from 'expo-constants';
-
-export const AWS_CONFIG = {
-  accessKeyId: Constants.expoConfig?.extra?.AWS_ACCESS_KEY_ID || '',
-  secretAccessKey: Constants.expoConfig?.extra?.AWS_SECRET_ACCESS_KEY || '',
-  region: Constants.expoConfig?.extra?.AWS_REGION || 'ap-northeast-2',
-  bucket: Constants.expoConfig?.extra?.AWS_S3_BUCKET || ''
-}; 
